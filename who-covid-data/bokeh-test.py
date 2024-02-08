0332a79de1c71f0b65e6da6978cb133166687601
jelleriecker <jelle.riecker@nelen-schuurmans.nl>
2024-02-08 18:33:26 +0100

    changed the x and y data

--- who-covid-data/bokeh-test.py
+++ who-covid-data/bokeh-test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from bokeh.plotting import figure, show
+from bokeh.models import YearsTicker
 
 # Data variables
 df = pd.read_csv("covid-19-global-data.csv")
@@ -9,14 +10,16 @@ df_arg = df.loc[df["Country"] == "Argentina", :]
 #df_arg = df.loc[(df['Country'] == 'Argentina') & (df['Date_reported'])]
 
 # Plot variables
-x = pd.to_datetime(df_arg['Date_reported'])
+x = pd.date_range(start='2019-1-1', periods=7, freq='YE')
 y = pd.to_numeric((df_arg['Cumulative_cases']))
 
-print(x, y)
+
 
 # from here we create the plot
-plot1 = figure()#title="cumulative cases Argentina", x_axis_label='Date reported', y_axis_label='cumulative cases')
-plot1.line(x, y)# legend_label="cases", line_width=2)
+plot1 = figure(x_axis_type="datetime", title="covid cases Argentina", plot_height=350, plot_width=800)#title="cumulative cases Argentina", x_axis_label='Date reported', y_axis_label='cumulative cases')
+plot1.xaxis.ticker = YearsTicker(interval=1)
+plot1.line(x, y)
 
 # Here we show the plot
 show(plot1)
+
